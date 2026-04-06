aef4d2981972e1b7a274eb225ba8094fbb37f612
svojas-a <svojasa.cks@gmail.com>
2026-04-06 22:32:30 +0530

    initial commit

---
+++ Item.java
@@ -0,0 +1,56 @@
+public class Item {
+    protected int itemId;
+    protected String name;
+    protected String description;
+    protected String dateReported;
+    protected String status;
+    protected String ownerName;
+    protected String contactInfo;
+    protected String location;
+
+    public Item(int itemId, String name, String description, String dateReported,
+                String ownerName, String contactInfo, String location) {
+
+        this.itemId = itemId;
+        this.name = name;
+        this.description = description;
+        this.dateReported = dateReported;
+        this.ownerName = ownerName;
+        this.contactInfo = contactInfo;
+        this.location = location;
+
+        this.status = "Reported";
+    }
+
+    public void updateStatus(String newStatus) {
+        this.status = newStatus;
+    }
+
+    public void displayItem() {
+        System.out.println("ID: " + itemId);
+        System.out.println("Name: " + name);
+        System.out.println("Description: " + description);
+        System.out.println("Date: " + dateReported);
+        System.out.println("Status: " + status);
+        System.out.println("Owner: " + ownerName);
+        System.out.println("Contact: " + contactInfo);
+        System.out.println("Location: " + location);
+        System.out.println("----------------------");
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+}
